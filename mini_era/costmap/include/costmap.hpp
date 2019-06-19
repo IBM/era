97c651bc020044700b6b07a02a905d69bef27570
Akin Sisbot <easisbot@us.ibm.com>
2019-06-19 12:18:02 -0400

    add costmap related code

--- mini_era/costmap/include/costmap.hpp
+++ mini_era/costmap/include/costmap.hpp
@@ -1,9 +1,44 @@
-#include <iostream>
-#include <stdlib.h>
+/*
+ * Copyright 2018 IBM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
+#include <octomap/octomap.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
 
 class costmap
 {
 public:
-	costmap();
+
+ 	std::unique_ptr<octomap::OcTree> octree;
+	pcl::PointCloud<pcl::PointXYZ> cloud;
+	octomap::pose6d cloud_origin;
+
+	costmap(double resolution);
+	int update();
+	int read_cloud(std::string path);
+	int write_octomap(std::string path);
+	int insert_cloud(pcl::PointCloud<pcl::PointXYZ> cloud, octomap::pose6d origin);
+	int test(std::string cloud_path, double x, double y, double z, double yaw, double pitch, double roll);
 };
+
+
+
+
+
+
+
+
--- mini_era/costmap/src/costmap.cpp
+++ mini_era/costmap/src/costmap.cpp
@@ -2,10 +2,95 @@
 #include <iostream>
 #include <stdlib.h>
 
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/io.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/filter.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/segmentation/progressive_morphological_filter.h>
+
 using namespace std;
+using namespace octomap;
 
-
-costmap::costmap()
+costmap::costmap(double resolution)
 {
-	cout << "test" <<endl;
+	octree = std::unique_ptr<octomap::OcTree>(new octomap::OcTree(resolution));
+	cout << "OcTree Created" <<endl;
+}
+
+int costmap::read_cloud(std::string path)
+{
+	pcl::io::loadPCDFile<pcl::PointXYZ>(path, cloud);
+
+}
+
+int costmap::write_octomap(std::string path)
+{
+  octree->writeBinary(path);
+
+}
+
+int costmap::insert_cloud(pcl::PointCloud<pcl::PointXYZ> cloud, pose6d origin)
+{
+	vector<int> indexes;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+	pcl::removeNaNFromPointCloud(cloud, *filtered_cloud, indexes);
+
+	octomap::Pointcloud octocloud;
+
+	for (pcl::PointCloud<pcl::PointXYZ>::const_iterator it = filtered_cloud->begin(); it != filtered_cloud->end(); ++it) {
+		point3d point(it->x, it->y, it->z);
+
+		octocloud.push_back(point);
+	}
+	point3d sensor_origin(0,0,0);
+
+	octree->insertPointCloud(octocloud, sensor_origin, origin);
+
+}
+
+int costmap::test(std::string cloud_path, double x, double y, double z, double yaw, double pitch, double roll)
+{
+	read_cloud(cloud_path);
+
+	insert_cloud(cloud, octomap::pose6d(x,y,z,roll,pitch,yaw));
+
+}
+
+
+int costmap::update()
+{
+	octomap::KeySet free_cells, occupied_cells;
+	octomap::KeyRay key_ray;
+	point3d origin(0,0,0);
+
+	for (pcl::PointCloud<pcl::PointXYZ>::const_iterator it = cloud.begin(); it != cloud.end(); ++it){
+		point3d point(it->x, it->y, it->z);
+
+		if (octree->computeRayKeys(origin, point, key_ray)){
+			free_cells.insert(key_ray.begin(), key_ray.end());
+		}
+
+		OcTreeKey key;
+    if (octree->coordToKeyChecked(point, key)){
+      occupied_cells.insert(key);
+    }
+	}
+
+	// mark free cells only if not seen occupied in this cloud
+  for(KeySet::iterator it = free_cells.begin(), end=free_cells.end(); it!= end; ++it){
+    if (occupied_cells.find(*it) == occupied_cells.end()){
+      octree->updateNode(*it, false);
+    }
+  }
+
+  // now mark all occupied cells:
+  for (KeySet::iterator it = occupied_cells.begin(), end=occupied_cells.end(); it!= end; it++) {
+    octree->updateNode(*it, true);
+  }
+
 }
