dc2685fb39aeb871e5b52105e6f2f8d9419d4346
Akin Sisbot <easisbot@us.ibm.com>
2020-09-24 13:48:03 -0400

    add call to costmap

--- src/main.c
+++ src/main.c
@@ -5,29 +5,78 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <signal.h>
 
-#define PORT 5555
+#include "occgrid.h"
 
+#define PORT 5556
+
+int sock = 0;
+
+float odometry[] = {0.0, 0.0, 0.0};
+
+void INThandler(int dummy)
+{
+	printf("Closing the connection\n");
+  close(sock);
+  exit(0);
+}
+
+float bytes_to_float(unsigned char * bytes)
+{
+	unsigned char b[] = {bytes[0], bytes[1], bytes[2], bytes[3]};
+	float f;
+	memcpy(&f, &b, sizeof(f));
+
+	return f;
+}
+
+int counter = 0;
+
+void write_array_to_file(unsigned char * data, long size)
+{
+	const int dimx = 50, dimy = 50;
+  int i, j;
+
+  char file_name[32];
+
+  snprintf(file_name,sizeof(char)*32, "image%d.ppm", counter);
+
+  FILE *fp = fopen(file_name, "w");
+  fprintf(fp, "P3 %d %d 255\n", dimx, dimy);
+
+  for (j = 0; j < dimy*dimx; ++j) {
+    fprintf(fp, " %d %d %d ", data[j], data[j], data[j]);
+   }
+
+  fclose(fp);
+  counter++;
+}
 
 void process_data(char* data, int data_size)
 {
 
+	unsigned char * grid = cloudToOccgrid((float*)data, data_size/sizeof(float),
+		odometry[0],odometry[1],odometry[2],1.5,
+		false,
+		0.05, 2.05,
+		100,
+		100, 100, 2.0,
+		254);
 
-
-
+	write_array_to_file(grid, 100/2.0*100/2.0);
 
 }
 
 
 int main(int argc, char *argv[])
 {
-
-	int sock = 0;
 	struct sockaddr_in servaddr;
 	char *ack = "OK";
 	unsigned char buffer[200000] = {0};
 
-	// socket create and verification
+
+	signal(SIGINT, INThandler);
 
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)  {
 		printf("Socket creation failed...\n");
@@ -78,15 +127,29 @@ int main(int argc, char *argv[])
 
 			process_data(buffer, total_bytes_read);
 
+		}
+
+		if(buffer[0] == 'O' && buffer[3]) {
+			int message_size = strtol(buffer+1, buffer+2, 10);
+			printf("expecting message size: %d\n", message_size);
+			send(sock, ack, 2, 0);
+
+			int total_bytes_read = 0;
+
+			valread = read(sock , buffer, 10000);
+			printf("read %d bytes\n", valread);
+
+
+			odometry[0] = bytes_to_float(buffer);
+			odometry[1] = bytes_to_float(buffer+4);
+			odometry[2] = bytes_to_float(buffer+8);
+
+			printf("odometry: %f %f %f\n", odometry[0], odometry[1], odometry[2]);
+
 
 		}
+
 	}
 
 	close(sock);
-
-
-
-
-
-
 }
--- src/read_bag.py
+++ src/read_bag.py
@@ -3,9 +3,10 @@
 import sys
 import rosbag
 import socket
+import struct
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 5555        # Port to listen on (non-privileged ports are > 1023)
+PORT = 5556        # Port to listen on (non-privileged ports are > 1023)
 
 bag_file = ""
 
@@ -14,17 +15,33 @@ def send_data(bag_file):
 	bag = rosbag.Bag(bag_file)
 
 	s =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 	s.bind((HOST, PORT))
 	s.listen(1)
 	conn, addr = s.accept()
 	print('Connected by', addr)
-	for topic, msg, t in bag.read_messages('/carla/hero1/lidar/lidar1/point_cloud'):
-		conn.sendall('L' + str(len(msg.data)) + 'L')
-		data = conn.recv(10)
-		print(data)
-		if data == 'OK' :
-			conn.sendall(msg.data)
-			print('sent ', len(msg.data))
+	for topic, msg, t in bag.read_messages(topics=['/carla/hero1/lidar/lidar1/point_cloud','/carla/hero1/odometry']):
+		if topic == '/carla/hero1/lidar/lidar1/point_cloud' and msg.data :
+
+			conn.sendall('L' + str(len(msg.data)) + 'L')
+			data = conn.recv(10)
+			print(data)
+			if data == 'OK' :
+				conn.sendall(msg.data)
+				print('sent ', len(msg.data))
+		if topic == '/carla/hero1/odometry' and msg.pose :
+			odometry = [msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z]
+
+			ba = bytearray(struct.pack("f", msg.pose.pose.position.x)) + bytearray(struct.pack("f", msg.pose.pose.position.y)) +	bytearray(struct.pack("f", msg.pose.pose.position.z))
+
+			conn.sendall('O' + str(len(ba)) + 'O')
+			data = conn.recv(10)
+			print(data)
+			if data == 'OK' :
+				conn.sendall(ba)
+				print('sent ', len(ba))
+
 
 	bag.close();
 
