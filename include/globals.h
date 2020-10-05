2d162e7de86e150d7b37b8685c4c02aac1c2e2c2
J-D Wellman <wellman@us.ibm.com>
2020-10-05 14:19:58 -0400

    Adding forgotten file?

---
+++ include/globals.h
@@ -0,0 +1,15 @@
+#ifndef GLOBALS_H_
+#define GLOBALS_H_
+
+#define DBGOUT(x) x  // Uncomment for DEBUG messages
+//#define DBGOUT(x)  // Uncomment for NO DEBUG messages
+
+//#define DBGOUT2(x) x  // Uncomment for DEBUG Level-2 (more detail) messages
+#define DBGOUT2(x)  // Uncomment for NO DEBUG Level-2 (more detail) messages
+
+//#define CHECK(x) x  // Uncomment for RUN-TIME CHECK logic
+#define CHECK(x)  // Uncomment for NO RUN-TIME CHECK logic
+
+void closeout_and_exit(int rv);
+
+#endif // GLOBALS_H_
